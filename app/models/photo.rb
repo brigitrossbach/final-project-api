581e113ce7083c22812112df61f4a7254b0c03a6
Brigit Rossbach <brigitrossbach@gmail.com>
2017-10-16 13:19:11 -0400

    changed photo serializer and model to have comments

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,5 @@ class Photo < ApplicationRecord
   belongs_to :user
   has_many :tags, dependent: :destroy
   has_many :comments
+  has_many :likes
 end
--- app/serializers/photo_serializer.rb
+++ app/serializers/photo_serializer.rb
@@ -1,10 +1,16 @@
 class PhotoSerializer < ActiveModel::Serializer
-  attributes :id, :url, :caption, :user_id, :username, :tags
+  attributes :id, :url, :caption, :user_id, :username, :tags, :likes_count
 
   has_many :comments
+  has_many :likes
+
 
   def username
     object.user.username
+  end
+
+  def likes_count
+    object.likes.count
   end
 
 end
