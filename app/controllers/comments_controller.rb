93a76eef69131f8c880700533f0608176a182b95
Brigit Rossbach <brigitrossbach@gmail.com>
2017-10-17 11:14:57 -0400

    refactored comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,9 +3,10 @@ class CommentsController < ApplicationController
   def create
       user = User.find(decoded_token[0]['user_id'])
       photo = Photo.find(params[:photoId])
+      photos = Photo.all
       comment = Comment.new(comment: params[:comment], photo_id: photo.id, user_id: user.id)
     comment.save
-    render json: comment
+    render json: photos
   end
 
 
